5c572e976fb31bc21a8ebd15cccffc52425e7a93
sxw66 <1748931836@qq.com>
2022-08-11 14:09:59 +0800

    添加菜单

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="homeBox">
     <components :is="componentOn" />
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    > 
-      <el-menu-item :index="index+''" v-for="(item,index) in tabledata" :key="index">{{item}}</el-menu-item>
-    </el-menu>
   </div>
 </template>
 
@@ -27,23 +19,12 @@ export default {
       }
     }
   },
-  data() {
-    return {
-      activeIndex: "1",
-      tabledata: ["首页", "菜单", "我的"]
-    };
-  },
   components: {
     adminComponent,
     userComponent
   },
   mounted() {
-    // window.console.log("刷新");
-  },
-  methods: {
-    handleSelect() {
-      window.console.log("刷新");
-    }
+    window.console.log("刷新");
   }
 };
 </script>
